64ac438fe9678a20303940685058247df9cb210e
Luis Lopez <llopez1518@gmail.com>
2022-03-31 13:56:26 -0600

    add blogs section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./index.css";
 import Hero from "./components/Hero";
 import SearchBar from "./components/SearchBar";
+import Blogs from "./components/Blogs";
 
 export default function App() {
   return (
     <>
       <Hero />
       <SearchBar />
+      <Blogs />
     </>
   );
 }
---
+++ src/components/Blogs.js
@@ -0,0 +1,37 @@
+import data from "../data";
+
+export default function Blogs() {
+  return (
+    <div className="blogs-container">
+      <h2 className="blog-title title-text">{data.blogTitle}</h2>
+      <h3 className="blog-sub sub-title-text">{data.blogSubTitle}</h3>
+      <div className="blog-posts">
+        <div className="blog">
+          <img
+            className="blog-image"
+            src={data.kitchen_image_01}
+            alt="Kitchen Remodel"
+          />
+          <h3 className="blog-link-text">Kitchen Remodel Project</h3>
+        </div>
+        <div className="blog">
+          <img
+            className="blog-image"
+            src={data.featured_01_img}
+            alt="Kitchen Remodel"
+          />
+          <h3 className="blog-link-text">First-Time Buyer Advice</h3>
+        </div>
+        <div className="blog">
+          <img
+            className="blog-image"
+            src={data.featured_03_img}
+            alt="Kitchen Remodel"
+          />
+          <h3 className="blog-link-text">Family Friendly Cities</h3>
+        </div>
+      </div>
+      <button className="blog-btn lrg-white-btn">{data.blogBtnText}</button>
+    </div>
+  );
+}
