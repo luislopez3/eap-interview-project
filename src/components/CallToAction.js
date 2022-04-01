4e7fb7bbd7d54b0ca005b35dce815d722b7880db
Luis Lopez <llopez1518@gmail.com>
2022-04-01 14:58:49 -0600

    add button to CTA component

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -5,6 +5,7 @@ export default function CallToAction() {
     <div className="cta-container">
       <h2 className="cta-title sub-title-text">{data.settingsCtaTitle}</h2>
       <h4>{data.settingsCtaText}</h4>
+      <button className="cta-btn lrg-white-btn">Get In Touch Now</button>
     </div>
   );
 }
