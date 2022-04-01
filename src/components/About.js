a20f3f9b71e15213fcd562faad6795c4e0f7a599
Luis Lopez <llopez1518@gmail.com>
2022-04-01 10:46:25 -0600

    format about component

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,13 @@ import data from "../data";
 export default function About() {
   return (
     <div className="about-container">
-      <h3 className="about-title-text sub-title-text">{data.settingsAboutTitle}</h3>
+      <h3 className="about-title-text sub-title-text">
+        {data.settingsAboutTitle}
+      </h3>
       <h4 className="about-text">{data.settingsAboutDesc}</h4>
-      <button className="about-btn lrg-white-btn">{data.settingsAboutBtnTxt}</button>
+      <button className="about-btn lrg-white-btn">
+        {data.settingsAboutBtnTxt}
+      </button>
     </div>
   );
 }
