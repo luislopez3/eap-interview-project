50602dd684ce8d754218b99c6bfb1007c347ae8b
Luis Lopez <llopez1518@gmail.com>
2022-04-02 11:11:38 -0600

    remove unused class names and style rules

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -3,10 +3,8 @@ import data from "../data";
 export default function Listings() {
   return (
     <div className="listings-container">
-      <h2 className="listings-title">{data.idxTilesTitle}</h2>
-      <h3 className="listings-sub-title">
-        {data.idxTilesSubTitle}
-      </h3>
+      <h2>{data.idxTilesTitle}</h2>
+      <h3>{data.idxTilesSubTitle}</h3>
       <div className="listings-box">
         <img src={data.settingsIdxPlaceholderImage} alt="Secondary" />
         {/* I wasn't sure how to use the data source below, 
